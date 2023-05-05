8ae7741afe0aacd2b1190e4ec56ec5a607155452
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-05 13:08:34 +0430

    implemented test.js file

---
+++ test.js
@@ -0,0 +1,20 @@
+
+
+
+
+console.log("Vaishnavi Dhanorkar..");
+
+var firstName = "Vaishnavi";
+console.log(firstName);
+
+var age; //Variable Declaration
+age = "22"; //variable Initialization
+console.log(age);
+
+var holder;  //Variable Declaration
+console.log(holder);  //Undefined
+holder ="My Speck";
+console.log(holder);
+
+holder = "Sachin Spec";  //Variable update
+console.log(holder);    
