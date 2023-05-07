58d85dcac46151af14d58323fe7769105fc7ff8c
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-07 09:36:55 +0430

    implemented 01_variable_assign.js file

---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+
+
+
+var myFullName;   //variable Declaration
+myFullName = "Vaishnavi  Dhanorkar"; //Variable Initialization 
+console.log(myFullName);     
+
+myFullName = "Vaishnavi Vishwasrao Dhanorkar";  //Variable update
+console.log(myFullName);    
+
+var myLove; //Variable Declaration
+myLove = "I love only JS"; //variable Initialization
+console.log(myLove);
+
+var list;   //variable Declaration
+list = "Vaishnavi Dhanorkar 22  Prabhat Colony, Shilangan Road, Amravati  444 601  Female  No "; //Variable Initialization 
+console.log(list);     
+
