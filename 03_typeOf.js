68fd3b7d151940d985497176ae35284c8423d9a2
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-08 05:56:48 +0430

    create 03_typeOf.js file

---
+++ 03_typeOf.js
@@ -0,0 +1,45 @@
+
+console.log("Before Initialization");
+var bankName;
+var typeOfBankName = typeof bankName;
+console.log( "Bank Name : ", bankName);
+console.log( "Data Type :  ", bankName);
+
+console.log("After Initialization");
+var bankName = "SBI Bank";
+var typeOfBankName = typeof bankName;
+console.log( "Bank Name : SBI Bank");
+console.log( "Data Type : ",typeOfBankName);
+console.log("=================================");
+
+var marks = "90%";
+var typeOfMarks = typeof marks;
+console.log( "Marks : 90%");
+console.log( "Data Type : ",typeOfMarks);
+console.log("=================================");
+
+console.log("Before Initialization");
+var value;
+var typeOfValue = typeof value;
+console.log( "value : " ,typeOfValue);
+
+console.log("After Initialization");
+var value = true;
+var isWorking = true;
+var typeOfValue = typeof value;
+console.log( "value : true");
+console.log( "Type of isWorking : ",typeOfValue);
+console.log("=================================");
+
+var isWorking = "Hundread and Seven";
+var typeOfIsWorking = typeof isWorking;
+console.log("value : Hundread and Seven ");
+console.log( "Type of isWorking : ",typeOfIsWorking);
+
+var isWorking = 107;
+var typeOfIsWorking = typeof isWorking;
+console.log("value : 107");
+
+console.log("type of isWorking :", typeOfIsWorking);
+
+
