8cefc59c407e99562d68821168525c1fa526f0eb
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-08 05:56:05 +0430

    Designed 02_variableBasicAssign.js file
    Designed 02_variableBasicAssign.js file

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,35 @@
+var books = 2;
+var bag = 5;
+var bottle = 4;
+var typeOfBooks = typeof books;
+console.log("Books: 2,  type:" ,typeOfBooks);
+
+var isPen = true;
+var isPencil = false; 
+var isLaptop = true;
+var typeOfIsLaptop = typeof isLaptop;
+console.log("isLaptop: true,  type:", typeOfIsLaptop);
+
+var studentName = "Vaishnavi";
+var collegeName = "DCPE, Amravati"; 
+var division = "MCA";
+var typeOfStudentName = typeof studentName;
+console.log( "StudentName:Vaishnavi,  type: ", typeOfStudentName );
+
+console.log("========================================"); 
+
+//Difference Between null vs undefined
+console.log("Difference Between Null vs Undefined");
+// 1. Undefined
+var fan;
+console.log("Value of variable Fan: ", fan);
+console.log("Type of variable fan: ", typeof fan);
+
+// 2. null
+var frize = null;
+console.log("Value of variable -> frize: ", frize);
+console.log('Type of variable frize is:', typeof frize);
+
+
+
+
