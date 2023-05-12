b173289617bbc96b7de9f71178d5af0bd3ba35a0
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-12 08:05:36 +0430

    Update 03_function.js

--- 03_function.js
+++ 03_function.js
@@ -27,3 +27,12 @@ function swapVariables(valueOne, valueTwo){ // Arguments: valueOne =100  valueTw
 swapVariables(numOne, numTwo); // 100 200
 swapVariables(name1, name2);
 swapVariables(fruitApple, fruitMango);
+
+function addition(num1, num2) {
+    var result  = num1 + num2;// result = 300
+    return result;
+}
+var returnValue = addition(100, 200);
+console.log("Addition of number is: ", returnValue);
+var returnResult = addition(890.400, 600.3456);
+console.log("Addition of number is: ", returnResult);
