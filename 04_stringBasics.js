43fa5aa8cd8c183f72b02525b171264e9aaa71fd
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-13 07:40:47 +0430

    create stringBasic.js

---
+++ 04_stringBasics.js
@@ -0,0 +1,15 @@
+ function stringBasics()
+{
+
+}
+
+ 
+    console.log("My dream company is Infosys");
+    var hobby1 = "Dansing";
+    var hobby2 = "Cooking";
+    var hobby3 = "Reading";
+    console.log(hobby1, hobby2, hobby3);
+    var result =hobby1.length + hobby2.length+hobby3.length;
+    
+    console.log("Sum of the Charactor that is available in hobby1, hobby2, hobby3 : ", result);
+    
