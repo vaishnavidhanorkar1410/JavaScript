a033b2bebd6d0c27016378c7eaf40a53929d7b14
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-15 16:59:27 +0430

    Create operatorA.js File

---
+++ 05_operatorA.js
@@ -0,0 +1,52 @@
+
+console.log(`Q1.`);
+
+function squareOfWordLength(){ 
+    console.log(`Invoke the function for the values JavaScript`);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    var firstWord = "JavaScript";
+    var value = 10;
+    var output = 10*10;
+    var result = firstWord.length;
+    console.log(`Length of the word is :  ${result}`);
+    console.log(`square of its Length is : ${output}`);
+    console.log(``);
+
+    console.log(`Invoke the function for the value Google Chrome`);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    var secondWord = "Google Chrome";
+    var value = 13;
+    var output = 13*13;
+    var result = secondWord.length;
+    console.log(`Length of the word is :  ${result}`);
+    console.log(`square of its Length is : ${output}`);
+    console.log(``);
+
+   console.log(`Invoke the function for the value Developer Smart`);
+    var thirdWord = "Developer Smart";
+    var value = 15;
+    var output = 15*15;
+    var result = thirdWord.length;
+    console.log(`Length of the word is :  ${result}`);
+    console.log(`square of its Length is : ${output}`);
+    console.log(``);
+    
+}
+squareOfWordLength();
+console.log(`Q2.`);
+
+
+function develop (){
+    var str = "I am Angular Developer";
+    var StrLength = str.length;
+    var resultSplit = str.split(" ");
+   // var totalWords = results.length;
+   var divide = str.length/resultSplit.length;
+   var multiply = str.length*resultSplit.length;
+   
+    console.log(`Given string is : ${str}`);
+    console.log(`Length of  given string is : ${StrLength}`);
+    console.log (`Total number of words available in that string is : ${resultSplit.length}`);
+    console.log (`The String length is divided by total number of words available in that string is : ${divide}`);
+    console.log (`The String length is divided by total number of words available in that string is : ${multiply}`);
+    
+}
+    develop();
