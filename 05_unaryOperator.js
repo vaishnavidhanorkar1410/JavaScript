e02cc2a9b597ec6733f871dbbe70262b3d53c266
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-17 07:42:04 +0430

    Create unaryOperator.js file

---
+++ 05_unaryOperator.js
@@ -0,0 +1,23 @@
+var num1 = 5;
+var num1 = num1 + 1;
+console.log(`${num1}`);
+
+console.log(`Increment operator - post fix`);
+var num2 = 10;
+var num2 = num2++;// num2 + 1 = 11 
+console.log(` post Increment operator:  ${num2}`);
+
+console.log(`Increment operator - pre fix`);
+var num3 = 15;
+var num3 = ++num3;// num3 + 1 = 16
+console.log(` post Increment operator:  ${num3}`);
+
+console.log(`Decrement operator - post fix`);
+var num4 = 20;
+var num4 = num4--;// num4 - 1 = 
+console.log(` post Increment operator:  ${num4}`);
+
+console.log(`Decrement operator - post fix`);
+var num5 = 30;
+var num5 = --num5;// num4 - 1 = 
+console.log(` post Increment operator:  ${num5}`);
