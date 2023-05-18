c48209deab72006d8b3aace2c3f0309e60bfd470
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-18 08:26:06 +0430

    Create ifStatement.js File

---
+++ 07_ifStatement.js
@@ -0,0 +1,18 @@
+console.log("I am going to office");
+
+var isTrafficToDailyRoute = false;
+if(isTrafficToDailyRoute){
+    console.log("Daily route");
+}
+
+console.log("Office");
+
+
+var num1 = 13;
+var isEven = num1%2==0;
+if (isEven) {  
+    console.log(` Even number: ${num1}`);
+} else {
+    console.log(` Odd number: ${num1}`);
+}
+console.log("After if else statement");
