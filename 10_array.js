e217f53bbe07f724e8cac72a33b5a126d19876da
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-05-24 07:44:44 +0430

    Update array.js file

--- 10_array.js
+++ 10_array.js
@@ -85,3 +85,58 @@ console.log(arrayOfRollNumbers);
 var arrayOfRollNumbers = [67, 56, 78, 99, 80];
 console.log(arrayOfRollNumbers);
 arrayOfRollNumbers.splice(2, 2, 111, 22, 333);
+
+console.log(`==== Traversing array =====`);
+
+var arrayNames = ["Anil", "Siya", "Ram" ,"Sunil", "Jenny"];
+for (let index = 0; index < arrayNames.length; index++) {
+    const element = arrayNames[index];
+    console.log(element);
+}
+
+
+// this show the index
+console.log(`------------ for in loop -----------------`);
+for (const index in arrayNames) {
+    console.log(arrayNames[index]);
+}
+
+
+// this show the element 
+console.log(`------------ for of loop -----------------`);
+for (const element of arrayNames) {
+    console.log(element);
+}
+
+
+
+console.log(`------- WAP to get even indexed element ------`);
+var arrayNames = ["Anil", "Siya", "Ram" ,"Sunil", "Jenny"];
+// WAP to get even indexed element
+for (const index in arrayNames) {
+   if (index%2==0) {
+    console.log(`${arrayNames[index]}`);
+   }
+}
+
+
+console.log(`---------- join() ------------`);
+let names = arrayNames.join(", ");
+console.log(names);
+
+console.log(`------------Concate the array---------------`);
+const arrayBoys = ["Anil", "Ram" ,"Sunil"];
+let arrayGirls = ["Siya", "Jenny"]
+
+let combineArray =  arrayBoys + arrayGirls;
+console.log(combineArray);
+
+let concatArray =  arrayBoys.concat(arrayGirls);
+console.log(concatArray);
+
+
+
+console.log(`------ Resize an array -------`);
+var arrayNames = ["Anil", "Siya", "Ram" ,"Sunil", "Jenny"];
+arrayNames.length = 3;
+console.log(arrayNames);
