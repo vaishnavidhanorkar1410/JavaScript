16fccbde773028fad96b4a5fad2c072c377c211d
vaishnavidhanorkar1410 <vaishnavidhanorkar1410@gmail.com>
2023-06-13 15:03:52 +0430

    create domAssign2.js

---
+++ DOM/domAssign2.js
@@ -0,0 +1,15 @@
+
+
+console.log('Add CSS properties');
+const elementProfile = document.querySelector('#profile');
+elementProfile.style.color = 'blue';
+
+console.log(`Removing an element Agile and JIRA`);
+const elementStack = document.querySelector('.stack');
+const elementJava = document.querySelector('#java');
+elementStack.removeChild(elementJava);
+
+
+console.log('Add CSS properties');
+const elementTech = document.querySelector('#tech');
+elementTech.style.color = 'red';
--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,6 @@
+
+
+
 const elementPersonal = document.getElementById("personal");
 console.log(elementPersonal.innerHTML);
 
@@ -29,6 +32,42 @@ for (const element of elementsPara) {
 
 console.log(`======== document.querySelectorAll(".ceo")`);
 const elementsCeo = document.querySelectorAll(".ceo")
+
 elementsCeo.forEach( (element)=>{
     console.log(element.innerHTML);
 } );
+
+console.log(` Updating an element city`);
+const elementCity = document.querySelector("#city");
+elementCity.innerHTML = "CITY: Mumbai";
+
+console.log(` Changing an element attribute`);
+const elementLink = document.querySelector('.link');
+elementLink.setAttribute('href', 'https://www.linkedin.com/login');
+
+
+console.log('Changing CSS properties');
+const elementAddress = document.querySelector('#address');
+elementAddress.style.color = 'red';
+elementAddress.style.fontFamily = 'sans-serif';
+
+console.log(`Removing an element`);
+const elementHobbies = document.querySelector('.hobbies');
+const elementCricket = document.querySelector('#cricket');
+elementHobbies.removeChild(elementCricket);
+
+console.log(`Creating node`);
+const elementTimePass = document.createElement('p');
+const textNodeTimePass = document.createTextNode("TimePass is also my hobby, Please don't laugh");
+elementTimePass.appendChild(textNodeTimePass);
+
+elementHobbies.appendChild(elementTimePass);
+
+const elementContact = document.querySelector('#contact');
+elementContact.addEventListener('click', ()=>{
+    elementContact.style.color = 'green';
+});
+
+function show(){
+    alert('show function called');
+}
